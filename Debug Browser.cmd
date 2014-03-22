da586038ea33d8259bdc8b44d5ea783e1f28dac6
jichi <jichifly@gmail.com>
2014-03-22 12:49:01 -0600

    save

---
+++ Debug Browser.cmd
@@ -0,0 +1,32 @@
+:: Debug Browser.cmd
+:: 11/1/2012 jichi
+@echo off
+setlocal
+
+cd /d %~dp0/..
+
+title Debug VNR
+::color 8f
+
+set ME=Debug VNR
+
+set PYTHON=Library\Frameworks\Python\python.exe
+set FLAGS=-B
+set PYTHON_OPT=
+set SCRIPT=Library/Frameworks/Sakura/py/apps/browser
+set SCRIPT_OPT=--debug
+
+set PATH=%windir%;%windir%\system32
+
+echo %ME%: PWD = %CD%
+echo %ME%: %PYTHON% %FLAGS% %PYTHON_OPT% %SCRIPT% %SCRIPT_OPT%
+echo.
+%PYTHON% %FLAGS% %PYTHON_OPT% %SCRIPT% %SCRIPT_OPT%
+set ret=%ERRORLEVEL%
+echo.
+echo %ME%: exit code: %ret%
+
+echo.
+pause
+
+:: EOF

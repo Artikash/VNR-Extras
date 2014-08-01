a44940405565794f839877dc3556bf6cf8017e2d
jichi <jichifly@gmail.com>
2014-07-31 21:28:27 -0500

    save

--- Debug Browser.cmd
+++ Debug Browser.cmd
@@ -19,9 +19,9 @@ set SCRIPT_OPT=--debug
 set PATH=%windir%;%windir%\system32
 
 echo %ME%: PWD = %CD%
-echo %ME%: %PYTHON% %FLAGS% %PYTHON_OPT% %SCRIPT% %SCRIPT_OPT%
+echo %ME%: %PYTHON% %FLAGS% %PYTHON_OPT% %SCRIPT% %SCRIPT_OPT% %*
 echo.
-%PYTHON% %FLAGS% %PYTHON_OPT% %SCRIPT% %SCRIPT_OPT%
+%PYTHON% %FLAGS% %PYTHON_OPT% %SCRIPT% %SCRIPT_OPT% %*
 set ret=%ERRORLEVEL%
 echo.
 echo %ME%: exit code: %ret%
